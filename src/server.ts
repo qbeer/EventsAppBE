403a67c26b39bbb93e1ea0ce38b377eac6a8e38e
Alex Olar <alex.olar@edreamsodigeo.com>
2018-08-03 16:18:10 +0200

    added cors

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import bodyParser from "body-parser";
+import cors from "cors";
 import express from "express";
 import { OAuth2Client } from "google-auth-library";
+import helmet from "helmet";
 import { PORT } from "../config";
 import { setUpRouter } from "./router";
 import { CalendarAuthService } from "./service/CalendarAuthService";
@@ -14,14 +16,22 @@ authService.authorizedClient().then((client: OAuth2Client) => {
 
     app.use(bodyParser.json());
 
+    app.use(cors({
+        origin: "https://makevents.herokuapp.com",
+        // tslint:disable-next-line:object-literal-sort-keys
+        optionsSuccessStatus: 200,
+    }));
+
+    app.use(helmet());
+
     app.use(setUpRouter(client));
 
     app.listen(port, () => {
         console.log(`Events App backend listening on port: ${port}`);
     });
 
-}).catch(() => {
+}).catch((err) => {
 
-    console.log("Authentication failed.");
+    console.log("Authentication failed." + err);
 
 });
